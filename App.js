46d7a3df4cf0fdbb2aab402d607075cf3cb23de7
Harshitb1 <36952154+Harshitb1@users.noreply.github.com>
2019-02-14 17:28:50 +0530

    native base

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Platform } from 'react-native';
+import {Button} from 'native-base';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Button><Text>Hello</Text></Button>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    flex:1,
+    marginTop: Platform.OS === 'android' ? 24 : 0
+  }
+}
