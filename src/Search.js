13d3ae913c717705d89ef9023c2821ac6755e966
Harshitb1 <36952154+Harshitb1@users.noreply.github.com>
2019-02-15 00:39:22 +0530

    pokeloader

--- src/Search.js
+++ src/Search.js
@@ -2,15 +2,35 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { Header, Item, Icon, Input, Button } from 'native-base';
+import SearchBody from './SearchBody';
+import PokeLoader from './PokeLoader';
 
 class Search extends React.Component {
 
 	state = {
+		onCall: true,
 		pokeSearch: ""
 	}
 
+	flipCall = () =>{
+		this.setState({onCall: !this.state.onCall});
+	}
+
+	renderBody = () =>{
+		if(this.state.onCall){
+			return(
+				<PokeLoader/>
+			)
+		}
+		else{
+			return(
+				<SearchBody flipCall={this.flipCall} data={this.state.data}/>
+			)
+		}
+	}
+
 	searchPoke = () =>{
-		
+
 	}
 
 	render() {
@@ -26,6 +46,7 @@ class Search extends React.Component {
                         />
                     </Item>
 				</Header>
+				{this.renderBody()}
 			</View>
 
 		)
---
+++ src/SearchBody.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View,Image,Text} from 'react-native';
+
+class SearchBody extends React.Component{
+	render(){
+	 
+	}
+}
+
+const styles = {
+
+}
+
+export default SearchBody;
