f2d6231ca39941d60c7a9a674ee2051732fcf4a8
Harshitb1 <36952154+Harshitb1@users.noreply.github.com>
2019-02-15 00:03:49 +0530

    searchbar

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,31 @@
 
 import React from 'react';
-import{View,Text} from 'react-native';
+import { View, Text } from 'react-native';
+import { Header, Item, Icon, Input, Button } from 'native-base';
 
-class Search extends React.Component{
-	render(){
-		return(
-			<View>
-       <Text>Search</Text>
+class Search extends React.Component {
+
+	state = {
+		pokeSearch: ""
+	}
+
+	searchPoke = () =>{
+		
+	}
+
+	render() {
+		return (
+			<View style={{ flex: 1 }}>
+			 	<Header searchBar rounded>
+                    <Item>
+                        <Icon name="ios-search" onPress={this.searchPoke}/>
+                        <Input 
+                        	value={this.state.pokeSearch}
+                        	placeholder="Search Pokemon"
+                        	onChangeText={(pokeSearch) => this.setState({pokeSearch})}
+                        />
+                    </Item>
+				</Header>
 			</View>
 
 		)
